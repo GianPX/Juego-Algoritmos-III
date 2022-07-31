6e5f02f2405bdb5531d7fc7ad4d8ffe83b9661d1
Nicola <ncamanfredi@gmail.com>
2022-07-30 20:06:04 -0400

    Creacion del puntaje

--- Juego Algoritmos III/src/Casilla.java
+++ Juego Algoritmos III/src/Casilla.java
@@ -1,79 +1,90 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Casilla<E extends Ficha,D,V extends Vecindad<D,Casilla>> {
-    //Attributes
+public class Casilla<E extends Ficha, D, V extends Vecindad<D, Casilla>> {
+    // Attributes
     E ficha;
     V vecinos;
-    //Constructor
-    public Casilla(E ficha, V vecinos){
-        this.ficha=ficha;
-        this.vecinos=vecinos;
-    }
-    //Methods
-    E getFicha(){
-        return this.ficha;
-    }
-    void setFicha(E ficha){
-        this.ficha=ficha;
+
+    // Constructor
+    public Casilla(E ficha, V vecinos) {
+        this.ficha = ficha;
+        this.vecinos = vecinos;
     }
 
-    public boolean insertar(Pieza pieza, E emptyFicha){
-        if(this.insertable(pieza.getDistribucion(),pieza.getRecorrido(),pieza.getPosiciones(),emptyFicha)){
-            this.insertar2(pieza.getDistribucion(),pieza.getRecorrido(),pieza.getPosiciones());
+    // Methods
+    E getFicha() {
+        return this.ficha;
+    }
+
+    void setFicha(E ficha) {
+        this.ficha = ficha;
+    }
+
+    public boolean insertar(Pieza pieza, E emptyFicha) {
+        if (this.insertable(pieza.getDistribucion(), pieza.getRecorrido(), pieza.getPosiciones(), emptyFicha)) {
+            this.insertar2(pieza.getDistribucion(), pieza.getRecorrido(), pieza.getPosiciones());
             return true;
-        }else {
+        } else {
             System.out.println("No se puede insertar la pieza");
             return false;
         }
     }
 
-    private boolean insertable(DistribucionPieza<D,E> distribucion, ArrayList<D> recorrido, ArrayList<D> posiciones,E emptyFicha){
+    private boolean insertable(DistribucionPieza<D, E> distribucion, ArrayList<D> recorrido, ArrayList<D> posiciones,
+            E emptyFicha) {
         ArrayList<Casilla> casillas = this.recorrer(recorrido);
-        if(casillas.size() == posiciones.size()){
-            for(Casilla<E,D,V> i : casillas){
+        if (casillas.size() == posiciones.size()) {
+            for (Casilla<E, D, V> i : casillas) {
                 E fichaP = distribucion.get(posiciones.get(0));
                 posiciones.remove(0);
                 E fichaC = i.getFicha();
-                if(!(fichaP.getValue().equals(emptyFicha.getValue()) || fichaC.getValue().equals(emptyFicha.getValue()))){
+                if (!(fichaP.getValue().equals(emptyFicha.getValue())
+                        || fichaC.getValue().equals(emptyFicha.getValue()))) {
                     return false;
                 }
             }
             return true;
-        } else return false;
+        } else
+            return false;
     }
-    public ArrayList<Casilla> recorrer(ArrayList<D> recorrido){
+
+    public ArrayList<Casilla> recorrer(ArrayList<D> recorrido) {
         recorrido.remove(0);
         ArrayList<Casilla> lista = new ArrayList<>();
-        if(recorrido.size()>0){
+        if (recorrido.size() > 0) {
             D clave = recorrido.get(0);
             Casilla next = this.vecinos.get(clave);
             lista.add(this);
-            if(next != null) lista.addAll(next.recorrer(recorrido));
+            if (next != null)
+                lista.addAll(next.recorrer(recorrido));
             return lista;
-        }else{
+        } else {
             lista.add(this);
             return lista;
         }
     }
-    public void recorrerProfundidad(ArrayList visitados){
+
+    public void recorrerProfundidad(ArrayList visitados) {
         visitados.add(this);
-        for(D i : this.vecinos.keySet()){
+        for (D i : this.vecinos.keySet()) {
             Casilla next = this.vecinos.get(i);
-            if(!visitados.contains(next)) next.recorrerProfundidad(visitados);
+            if (!visitados.contains(next))
+                next.recorrerProfundidad(visitados);
         }
-        
+
     }
-    private void insertar2(DistribucionPieza<D,E> distribucion, ArrayList<D> recorrido, ArrayList<D> posiciones){
+
+    private void insertar2(DistribucionPieza<D, E> distribucion, ArrayList<D> recorrido, ArrayList<D> posiciones) {
         ArrayList<Casilla> casillas = this.recorrer(recorrido);
-        for(Casilla<E,D,V> i : casillas){
-            
+        for (Casilla<E, D, V> i : casillas) {
+
             i.setFicha(distribucion.get(posiciones.get(0)));
             posiciones.remove(0);
         }
     }
-    
-    public void setVecinos(V vecinos){
-        this.vecinos=vecinos;
+
+    public void setVecinos(V vecinos) {
+        this.vecinos = vecinos;
     }
 }
---
+++ Juego Algoritmos III/src/Puntaje.java
@@ -0,0 +1,26 @@
+
+public class Puntaje {
+    int puntos;
+
+    public int contarPuntaje(Pieza pieza, int ch, Ficha emptyFicha) {
+
+        this.puntos += pieza.getFichasPieza(emptyFicha);
+
+        if (ch != 0) {
+            this.puntos += 60 *ch*ch;
+        }
+
+        return this.puntos;
+    }
+    
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+
+    public int getPuntos() {
+        return this.puntos;
+    }
+
+}
